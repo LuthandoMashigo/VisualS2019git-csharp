97be97bdda36df38c147aeb8e427e8a844b7ac32
Young <youngking.ak4787@gmail.com>
2021-10-04 12:53:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualS2019git_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
